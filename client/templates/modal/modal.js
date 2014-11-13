8c77c4a611477555053e4870ce966e4c2c2d57c8
flemdizzle <fleminsh@gmail.com>
2014-11-13 14:40:26 -0500

    got the collection inputs worked out for user info

--- client/templates/modal/modal.js
+++ client/templates/modal/modal.js
@@ -4,7 +4,13 @@ Template.modal.events({
         $('#myModal').modal('toggle');
         $.each($('.checked-modal'), function(key, value){
 			Tasks.remove($(this).data("id"));
-			console.log($(this).text());
+			// need to add insert new user data
+			Completed.insert({
+				text:$(this).text(),
+				completedAt: new Date(),
+				owner: Meteor.userId(),
+				username: Meteor.user().username
+			});
         });
     }
 });
--- client/timer_app.js
+++ client/timer_app.js
@@ -8,7 +8,9 @@ if (Meteor.isClient) {
 
       Tasks.insert({
         text:text,
-        createdAt: new Date()
+        createdAt: new Date(),
+        owner: Meteor.userId(),
+        username: Meteor.user().username
       });
 
       event.target.text.value = "";
